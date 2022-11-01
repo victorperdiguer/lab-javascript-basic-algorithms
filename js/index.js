2ae971410eb472e8b7bd39bd44de70472a6e2a73
Victor Perdiguer <victor.perdiguer@gmail.com>
2022-11-01 21:14:01 +0100

    first go at lab, no console tests yet

--- js/index.js
+++ js/index.js
@@ -1,6 +1,38 @@
 // Iteration 1: Names and Input
-//
+
+const hacker1 = "Fernando Alonso";
+console.log(`The driver's name is ${hacker1}`);
+const hacker2 = "Alpine F1 Racing Team";
+console.log(`The navigator's name is ${hacker2}`);
+
 // Iteration 2: Conditionals
 
+if (hacker1.length > hacker2.length){
+    console.log(`The driver has the longest name, it has ${hacker1.length} characters.`);
+};
+if (hacker1.length < hacker2.length){
+    console.log(`The navigator has the longest name, it has ${hacker2.length} characters.`);
+};
+if (hacker1.length === hacker2.length){
+    console.log(`Wow, you both have equally long names, ${hacker2.length} characters!`);
+};
 
 // Iteration 3: Loops
+
+//3.1
+let bigNameWithSpaces;
+for (letter in hacker1){
+    bigNameWithSpaces = bigNameWithSpaces + letter + " ";
+};
+bigNameWithSpaces = bigNameWithSpaces.toUpperCase();
+bigNameWithSpaces.trim();
+console.log(bigNameWithSpaces);
+
+//3.2
+let reverseName;
+for (i = hacker2.length; i--; i==0){
+    reverseName = reverseName + hacker2[i];
+};
+console.log(reverseName);
+
+//3.3
